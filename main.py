295c42d2150061902c89c21dc85542ad985b2ea8
Christian-Bull <cbull.projects@gmail.com>
2025-04-22 10:06:08 -0600

    create new dataset for aggregated by day

--- main.py
+++ main.py
@@ -3,5 +3,21 @@ import pandas as pd
 # Read the CSV file
 bikeshare_data = pd.read_csv("data/london_merged_hour.csv")
 
-# View the first 5 rows
-print(bikeshare_data.head(20))
+bikeshare_data['timestamp'] = pd.to_datetime(bikeshare_data['timestamp'])
+
+# Set the date (without time) as a new column
+bikeshare_data['date'] = bikeshare_data['timestamp'].dt.date
+
+# Aggregate the data by day
+daily_bikeshare_data = bikeshare_data.groupby('date').agg({
+    'cnt': 'sum',
+    't1': 'mean',
+    't2': 'mean',
+    'hum': 'mean',
+    'wind_speed': 'mean',
+    'weather_code': lambda x: x.mode()[0] if not x.mode().empty else x.iloc[0],
+    'is_holiday': 'max',
+    'is_weekend': 'max',
+    'season': 'max'
+}).reset_index()
+
