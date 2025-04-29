7322c2adac050e3bbd7af6e4084a85d343647a74
Christian-Bull <cbull.projects@gmail.com>
2025-04-29 14:15:29 -0600

    add jupyter notebook

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
+import numpy as np
+import scipy as sp
+import scipy.stats as stats
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+# Set color map to have light blue background
+sns.set()
+import statsmodels.formula.api as smf
+import statsmodels.api as sm
 
 # Read the CSV file
 bikeshare_data = pd.read_csv("data/london_merged_hour.csv")
@@ -21,3 +30,22 @@ daily_bikeshare_data = bikeshare_data.groupby('date').agg({
     'season': 'max'
 }).reset_index()
 
+print(bikeshare_data.head(10))
+
+test_data = daily_bikeshare_data[:100]
+
+best_guess_predictor = {}
+
+predictors = test_data.columns.tolist()
+
+print(predictors)
+for predictor in predictors:
+    model = smf.ols(formula=f'cnt ~ {predictor}', data=test_data)
+    results = model.fit()
+    best_guess_predictor[predictor] = results.rsquared
+
+best_guess_pred_sorted = {k: v for k, v in sorted(best_guess_predictor.items(), key=lambda item: item[1])}
+
+print(best_guess_pred_sorted)
+
+print(test_data.head())
